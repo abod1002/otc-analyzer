54136e258d65d5e7605c8a174e49f4f47bd0f756
abod1002 <oraini1002@gmail.com>
2025-05-06 01:52:53 +0300

    main.py

--- main.py
+++ main.py
@@ -8,3 +8,5 @@ templates = Jinja2Templates(directory="templates")
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+app = FastAPI()
