ee9ac293314e0f61f8b97b09d15926b7a4eb6a4a
abod1002 <oraini1002@gmail.com>
2025-05-06 13:31:27 +0300

    main.py

--- main.py
+++ main.py
@@ -2,17 +2,17 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 import asyncio
-from websocket_client import connect_to_socket
+from websocket_client import start_socket  # استيراد سكربت WebSocket
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
+# تشغيل WebSocket عند بدء التطبيق
+@app.on_event("startup")
+async def startup_event():
+    asyncio.create_task(start_socket())
+
+# الصفحة الرئيسية للموقع
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
-
-@app.on_event("startup")
-async def start_socket():
-    asyncio.create_task(connect_to_socket())
-
-# Force deploy
